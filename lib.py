a534c7e099962873bba02d029d60b2acdedb3f74
Sergi Romeu <romeuhd@gmail.com>
2020-04-10 12:06:41 +0200

    add example in lib

--- lib.py
+++ lib.py
@@ -14,3 +14,9 @@ def get_first_unique_letter(input = ''):
     return counter[0]
   except IndexError:
     return None
+
+if __name__ == '__main__':
+  print('AAB ->', get_first_unique_letter('AAB'))
+  print('CBA ->', get_first_unique_letter('CBA'))
+  print('ABAC ->', get_first_unique_letter('ABAC'))
+  print('ZXYXZ ->', get_first_unique_letter('ZXYXZ'))
